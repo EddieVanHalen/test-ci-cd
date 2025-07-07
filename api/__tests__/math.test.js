1c0a9f4af3e49417f659d1c6c273053858f96c4f
EddieVanHalen <atillaismayil94@gmail.com>
2025-07-07 20:00:02 +0400

    Third

--- api/__tests__/math.test.js
+++ api/__tests__/math.test.js
@@ -1,11 +1,14 @@
-function division(a, b){
-    return a/b;
+function division(a, b) {
+  return a / b;
 }
 
-test('10 / 2 equal 5', ()=>{
-    expect(division(10, 2)).toBe(5)
-})
+console.log("asd");
 
-test('15 / 3 equal 5', ()=>{
-    expect(division(15, 3)).toBe(5)
-})
+test("10 / 2 equal 5", () => {
+  expect(division(10, 2)).toBe(5);
+});
+
+test("15 / 3 equal 5", () => {
+  expect(division(15, 3)).toBe(5);
+});
+
